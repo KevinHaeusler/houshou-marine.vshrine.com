be7af186fc79995a5dbfb6248ec9d19b912c14ab
Kevin Haeusler <kevin.haeusler@khdev.ch>
2021-04-12 13:13:12 +0200

    create nuxt app and theme colors

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="w-screen ">
+    <div class="mx-auto max-w-screen-lg ">
+      <h1 class="text-primary text-center font-bold text-5xl">
+       <span>Houshou</span> <span class="text-primary-dark">Marine</span> <span class="text-secondary">Virtual</span> <span class="text-accent">Shrine</span>
+      </h1>
+      <div class="flex mt-5 place-content-center">
+      <div class="w-40 h-40 mr-5 bg-primary text-white text-center"> 
+        Primary  
+      </div>
+            <div class="w-40 h-40 mr-5 bg-primary-dark text-white text-center"> 
+        Primary Dark 
+      </div>
+      <div class="w-40 h-40 mr-5 bg-secondary text-white text-center ">   
+        Secondary
+      </div>
+      <div class="w-40 h-40 mr-5 bg-accent text-white text-center">  
+        Accent 
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {colors: {
+      primary: {
+        DEFAULT : '#8C0808',
+        dark: '#590303'
+
+      },
+      secondary: '#BF657D',
+      accent: '#F2DB94',
+    },},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
